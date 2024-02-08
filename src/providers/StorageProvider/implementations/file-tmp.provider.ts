7cc7bca72dac09090773e4ee8018809c2e8103c5
Kaio Moreira <kaio-dev@outlook.com>
2024-02-08 10:19:40 -0300

    add: log for debug core upload image

--- src/providers/StorageProvider/implementations/file-tmp.provider.ts
+++ src/providers/StorageProvider/implementations/file-tmp.provider.ts
@@ -4,32 +4,30 @@ import fs from 'fs'
 import "dotenv/config"
 
 export class FileTMPProvider implements IFileProvider{
-    deleteFileTmp(fileName: string, folderPath: string){
+    deleteFileTmp(fileName: string, folderPath: string, destination: string){
         try {
-            // verifica qual pasta é em produção ou em dev pois pode ser src ou build
-            let path = env.NODE_ENV === 'development' ? env.FOLDER_TMP_DEVELOPMENT : env.FOLDER_TMP_PRODUCTION
 
             if(env.NODE_ENV === 'test'){
-                path = env.FOLDER_TMP_DEVELOPMENT
+                destination = env.FOLDER_TMP_DEVELOPMENT
             }
             if(folderPath === 'tmp'){
                 // Verifique se o arquivo existe antes de tentar excluí-lo
                 // Verifique se o arquivo existe antes de tentar excluí-lo
-                if (!fs.existsSync(`${path}/${fileName}`)) {
+                if (!fs.existsSync(`${destination}/${fileName}`)) {
                     return
                 }
 
                 // Exclua o arquivo
-                return fs.unlinkSync(`${path}/${fileName}`);
+                return fs.unlinkSync(`${destination}/${fileName}`);
             }
 
             // Verifique se o arquivo existe antes de tentar excluí-lo
-            if (!fs.existsSync(`${path}/${folderPath}/${fileName}`)) {
+            if (!fs.existsSync(`${destination}/${folderPath}/${fileName}`)) {
                 return
             }
 
             // Exclua o arquivo
-            fs.unlinkSync(`${path}/${folderPath}/${fileName}`);
+            fs.unlinkSync(`${destination}/${folderPath}/${fileName}`);
         } catch (error) {
             throw error
         }
