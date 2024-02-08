becc3d6c3c782193628b1f9b66dee09ee9539f97
Kaio Moreira <kaio-dev@outlook.com>
2024-02-08 10:19:40 -0300

    add: log for debug core upload image

--- src/usecases/images/upload-to-order/upload-images-to-order-usecase.ts
+++ src/usecases/images/upload-to-order/upload-images-to-order-usecase.ts
@@ -29,25 +29,33 @@ export class UploadImageToOrderUseCase {
     }: IRequestUploadImage): Promise<IImageModel[]>{
         // comprimir a imagem com o sharp antes de fazer upload no firebase
         for(let image of imageInfo){
+            console.log('comprimir imagem')
             makeCompressionImage(image.hashName, image.destination, 'orders')
         }
 
+        console.log('buscar pedido no banco de dados')
         const findOrderExists = await this.ordersRepository.findById(idOrder)
         
         if(!findOrderExists){
             throw new AppError('Pedido não encontrado', 404)
         }
 
+        console.log('fazer upload da imagem')
         // lista de imagens
         let arrayImagesUploaded: IImageModel[] = []
         // criar for para fazer upload de mais de uma imagem no firebase storage
         // e salvar cada url na tabela de imagens
         for(let image of imageInfo){
+            console.log('formatar nome da imagem')
             const formatName = `${image.hashName.replace(/\..+$/, ".webp")}`
+            console.log(formatName)
 
+            console.log('fazer upload da imagem storage')
             // fazer upload do exame dentro firebase através do nome do arquivo
             let imageUrl = await this.storageProvider.uploadFile(formatName, `${image.destination}/orders`, 'orders') as string
             // criar imagem no banco de dados
+
+            console.log('criar imagem info no banco')
             const createImage = await this.imageRepository.upload({
                idOrder,
                name: image.name.replace(/\..+$/, ".webp"),
@@ -58,8 +66,9 @@ export class UploadImageToOrderUseCase {
             // adicionar imagem no array de imagens
             arrayImagesUploaded.push(createImage)
 
+            console.log('deleta imagem antiga no tmp')
             // deletar imagem não comprimida no tmp
-            this.fileProvider.deleteFileTmp(image.hashName as string, 'tmp')
+            this.fileProvider.deleteFileTmp(image.hashName as string, 'tmp', image.destination)
         }
 
         // retornar array de imagens
