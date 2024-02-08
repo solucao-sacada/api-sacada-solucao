2281c9c5a9139ca91e83173f22a1c07ca6b4425f
Kaio Moreira <kaio-dev@outlook.com>
2024-02-08 16:01:38 -0300

    add: log for debug core upload image

--- src/usecases/images/upload-to-order/upload-images-to-order-usecase.ts
+++ src/usecases/images/upload-to-order/upload-images-to-order-usecase.ts
@@ -34,7 +34,9 @@ export class UploadImageToOrderUseCase {
         if(!findOrderExists){
             throw new AppError('Pedido não encontrado', 404)
         }
-console.log(JSON.stringify(imageInfo, null, 2))
+        
+        console.log(JSON.stringify(imageInfo, null, 2))
+
         console.log('fazer upload da imagem')
         // lista de imagens
         let arrayImagesUploaded: IImageModel[] = []
@@ -44,32 +46,32 @@ console.log(JSON.stringify(imageInfo, null, 2))
             console.log('formatar nome da imagem')
             // const formatName = `${image.hashName.replace(/\..+$/, ".webp")}`
 
-            const x = fs.existsSync(`${image.destination}/${image.hashName}`)
-            console.log(x)
+            // const x = fs.existsSync(`${image.destination}/${image.hashName}`)
+            // console.log(x)
 
             // if(fs.existsSync(image.hashName)){}
-
+            console.log(image.destination)
             console.log(image.hashName)
 
             console.log('fazer upload da imagem storage')
             // fazer upload do exame dentro firebase através do nome do arquivo
             let imageUrl = await this.storageProvider.uploadFile(image.hashName, `${image.destination}`, 'orders') as string
             // criar imagem no banco de dados
+            console.log(imageUrl)
+            // console.log('criar imagem info no banco')
+            // const createImage = await this.imageRepository.upload({
+            //    idOrder,
+            //    name: image.name,
+            //    hashName: image.hashName,
+            //    url: imageUrl
+            // })
 
-            console.log('criar imagem info no banco')
-            const createImage = await this.imageRepository.upload({
-               idOrder,
-               name: image.name,
-               hashName: image.hashName,
-               url: imageUrl
-            })
+            // // adicionar imagem no array de imagens
+            // arrayImagesUploaded.push(createImage)
 
-            // adicionar imagem no array de imagens
-            arrayImagesUploaded.push(createImage)
-
-            console.log('deleta imagem antiga no tmp')
-            // deletar imagem não comprimida no tmp
-            this.fileProvider.deleteFileTmp(image.hashName as string, 'tmp', image.destination)
+            // console.log('deleta imagem antiga no tmp')
+            // // deletar imagem não comprimida no tmp
+            // this.fileProvider.deleteFileTmp(image.hashName as string, 'tmp', image.destination)
         }
 
         // retornar array de imagens
