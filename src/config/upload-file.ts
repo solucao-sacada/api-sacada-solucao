20457c13d9500d22bd7f6c67feaf367360298f4b
Kaio Moreira <kaio-dev@outlook.com>
2024-04-01 14:08:04 -0300

    fix: bug dont up image for production

--- src/config/upload-file.ts
+++ src/config/upload-file.ts
@@ -12,7 +12,7 @@ const getDirectories = (source: string): string[] =>
 
 // Variável tmpFolder recebe o caminho da pasta tmp
 // const tmpFolder = resolve(__dirname, "..", "..", "src");
-const envTmpFolder = env.NODE_ENV === "production" ? `${env.FOLDER_TMP_PRODUCTION}` : `${env.FOLDER_TMP_DEVELOPMENT}`
+const envTmpFolder = env.NODE_ENV === "development" ? "./src/tmp" : "./build/tmp";
 const tmpFolder = resolve(__dirname, "..", "..", envTmpFolder);
 
 // Cria array de string vazia com
