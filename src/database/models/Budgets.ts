150dfcab9a22322eb186037eba91898ee6a2ab22
Kaio Moreira <kaio-dev@outlook.com>
2024-04-10 11:03:57 -0300

    add: code for orçamento

--- src/database/models/Budgets.ts
+++ src/database/models/Budgets.ts
@@ -2,6 +2,7 @@ import { Schema, model, Document } from "mongoose";
 
 export interface IBudGetModel extends Document {
   idUser: Schema.Types.ObjectId
+  code: number;
   client: string
   emailClient: string          
   price: number
@@ -9,6 +10,11 @@ export interface IBudGetModel extends Document {
 
 export const BudGetSchema = new Schema<IBudGetModel>({
     idUser: { type: Schema.Types.ObjectId, ref: "Users", required: true},
+    code: {
+        type: Number,
+        unique: true,
+        required: false
+    },
     client: {
         type: String,
         required: true
