df77c093105142f1d2aee2b5ece8581d5293ebcb
Kaio Moreira <kaio-dev@outlook.com>
2024-05-10 11:56:19 -0300

    add: comapny for return login

--- src/usecases/factories/users/make-login-user-usecase.ts
+++ src/usecases/factories/users/make-login-user-usecase.ts
@@ -1,4 +1,5 @@
 import { DayjsDateProvider } from "@/providers/DateProvider/implementations/provider-dayjs";
+import { MongooseCompanyRepository } from "@/repositories/mongoose/mongoonse-company-repository";
 import { MongooseTokensRepository } from "@/repositories/mongoose/mongoose-tokens-repository";
 import { MongooseUsersRepository } from "@/repositories/mongoose/mongoose-users-repository";
 import { LoginUseCase } from "@/usecases/users/login/login-usecase";
@@ -7,10 +8,13 @@ export async function makeLoginUser(): Promise<LoginUseCase> {
     const usersRepository = new MongooseUsersRepository();
     const usersTokensRepository = new MongooseTokensRepository();
     const dayjsDateProvider = new DayjsDateProvider();
+    const companysRepository = new MongooseCompanyRepository();
+    
     const loginUseCase = new LoginUseCase(
         usersRepository,
         usersTokensRepository,
         dayjsDateProvider,
+        companysRepository
     )
 
     return loginUseCase
