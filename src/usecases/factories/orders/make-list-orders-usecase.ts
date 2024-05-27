de069b2899544d25677633877cf56e476bb42efe
Kaio Moreira <kaio-dev@outlook.com>
2024-05-27 12:19:09 -0300

    fix: json for equal url json

--- src/usecases/factories/orders/make-list-orders-usecase.ts
+++ src/usecases/factories/orders/make-list-orders-usecase.ts
@@ -1,10 +1,14 @@
+import { MongooseImagesRepository } from "@/repositories/mongoose/mongoose-images-repository";
 import { MongooseOrdersRepository } from "@/repositories/mongoose/mongoose-orders-repository";
 import { ListOrdersUseCase } from "@/usecases/orders/list/list-orders-usecase";
 
 export async function makeListOrder(): Promise<ListOrdersUseCase> {
     const orderRepository = new MongooseOrdersRepository()
+    const imageRepository = new MongooseImagesRepository()
+
     const listOrdersUseCase = new ListOrdersUseCase(
         orderRepository,
+        imageRepository
     )
 
     return listOrdersUseCase
